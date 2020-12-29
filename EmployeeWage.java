d473183809c634579944a77a371bb00caa1a75ac
Karishma2802 <Karishmaneha2015@gmail.com>
2020-12-29 11:24:25 +0530

    AddingPartTimeEmployeeAndWage_UC3

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,16 +3,21 @@ public class EmployeeWage
 	public static void main(String[] args)
 	{
 		int fullTime=1;
+		int partTime=2;
 		int empHrs=0;
 		int empRatePerHr=20;
 		int salary=0;
-		int Result=(int)(Math.random()*10)%2;
+		int Result=(int)(Math.random()*10)%3;
 
-		System.out.println("Result= " +Result);
+//		System.out.println("Result= " +Result);
 
 		if(Result == fullTime)
 		{
 			empHrs=8;
+		}
+		else if(Result == partTime)
+		{
+			empHrs=4;
 		}
 		else
 		{
