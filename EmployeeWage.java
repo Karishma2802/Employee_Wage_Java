e8d5675805a453e0e5abc99b3a754755f9654f3a
Karishma2802 <Karishmaneha2015@gmail.com>
2020-12-29 11:34:34 +0530

    CalculatingWagesForAMonth_UC5

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,28 +3,36 @@ public class EmployeeWage
 	public static final int FULL_TIME=1;
 	public static final int PART_TIME=2;
 	public static final int EMP_RATE_PER_HR=20;
+	public static final int NUM_OF_WORKING_DAYS=20;
 
 	public static void main(String[] args)
 	{
 		int empHrs=0;
-		int salary=0;
-		int empCheck=(int)(Math.random()*10)%3;
+		int totalEmpWage=0;
+		int empWage=0;
 
-		switch(empCheck)
+		for(int day=0;day<NUM_OF_WORKING_DAYS;day++)
 		{
-			case FULL_TIME:
-			empHrs=8;
-			break;
+			int empCheck=(int)(Math.random()*10)%3;
 
-			case PART_TIME:
-			empHrs=4;
-			break;
+			switch(empCheck)
+			{
+				case FULL_TIME:
+				empHrs=8;
+				break;
 
-			default:
-			empHrs=0;
+				case PART_TIME:
+				empHrs=4;
+				break;
+
+				default:
+				empHrs=0;
+			}
+			empWage=empHrs*EMP_RATE_PER_HR;
+			totalEmpWage+=empWage;
+//			System.out.println("Per day wage: "+empWage);
 		}
 
-		salary=empHrs*EMP_RATE_PER_HR;
-		System.out.println("Salary is "+salary);
+		System.out.println("Total Employee wage is : "+totalEmpWage);
 		}
 }
